605c504e72d1847d4423c8ca696d20a28012336e
agusahumada <agustinaahumada649@gmail.com>
2020-07-01 23:01:19 -0300

    Agregando y quitando atributos

---
+++ EjercicioJs3/ejercicio3.js
@@ -0,0 +1,7 @@
+//removiendo enlaces
+const enlaces = document.querySelectorAll(".enlace");
+const navegacion = document.querySelector("#principal");
+
+enlaces[0].remove();
+
+console.log(enlaces);
---
+++ EjercicioJs3/ejercicio3Part2.js
@@ -0,0 +1,31 @@
+// Agregando y modificando clases y atributos.
+
+const primerLi = document.querySelector(".enlace");
+
+let elemento;
+
+//obtener una clase de CSS
+elemento = primerLi.className;
+
+//agregando clase
+elemento = primerLi.classList.add("nueva-clase");
+
+//removiendo clase
+elemento = primerLi.classList.remove("nueva-clase");
+elemento = primerLi.classList;
+
+//leer atributos
+elemento = primerLi.getAttribute("href");
+
+//modificando o añadiendo un atributo
+primerLi.setAttribute("href", "https://google.com");
+primerLi.setAttribute("data-id", 30);
+
+//para comprobar que tenga atributo
+elemento = primerLi.hasAttribute("data-id");
+
+//removiendo atributos
+primerLi.removeAttribute("data-id");
+
+elemento = primerLi;
+console.log(elemento);
