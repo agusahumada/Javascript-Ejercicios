389046235d3bd8318be05e9dc39dd59dbd381435
agusahumada <agustinaahumada649@gmail.com>
2020-07-03 03:16:51 -0300

    Event Listener Click

---
+++ Seccion- Event-Listeners/App.js
@@ -0,0 +1,10 @@
+//Event listener click al buscador
+
+document
+  .querySelector("#submit-buscador")
+  .addEventListener("click", ejecutarBoton);
+
+function ejecutarBoton(e) {
+  e.preventDefault();
+  console.log("Desde la funcion ejecutar boton");
+}
