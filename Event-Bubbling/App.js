d8300b252458fd7d34f4577fb633de61c2167f0b
agusahumada <agustinaahumada649@gmail.com>
2020-07-04 23:58:14 -0300

    Event Bubbling

---
+++ Event-Bubbling/App.js
@@ -0,0 +1,23 @@
+// Event bubbling
+
+const card = document.querySelector(".card");
+const infoCurso = document.querySelector(".info-card");
+const agregarCarrito = document.querySelector(".agregar-carrito");
+
+card.addEventListener("click", function (e) {
+  console.log("Click en card");
+
+  //Para detener la propagacion
+
+  e.stopPropagation();
+});
+
+infoCurso.addEventListener("click", function (e) {
+  console.log("Click en info Curso");
+  e.stopPropagation();
+});
+
+agregarCarrito.addEventListener("click", function (e) {
+  console.log("Click en agregar a carrito");
+  e.stopPropagation();
+});
--- Eventos-para-los-inputs/app.js
+++ Eventos-para-los-inputs/app.js
@@ -1,16 +1,16 @@
-const busqueda = document.querySelector("#buscador");
+// const busqueda = document.querySelector("#buscador");
 
-// busqueda.addEventListener("keydown", obtenerEvento);
-// busqueda.addEventListener("keyup", obtenerEvento);
-// busqueda.addEventListener("keypress", obtenerEvento);
-// busqueda.addEventListener("focus", obtenerEvento);
-// busqueda.addEventListener("blur", obtenerEvento);
-// busqueda.addEventListener("cut", obtenerEvento);
-// busqueda.addEventListener("copy", obtenerEvento);
+// // busqueda.addEventListener("keydown", obtenerEvento);
+// // busqueda.addEventListener("keyup", obtenerEvento);
+// // busqueda.addEventListener("keypress", obtenerEvento);
+// // busqueda.addEventListener("focus", obtenerEvento);
+// // busqueda.addEventListener("blur", obtenerEvento);
+// // busqueda.addEventListener("cut", obtenerEvento);
+// // busqueda.addEventListener("copy", obtenerEvento);
 
-busqueda.addEventListener("input", obtenerEvento);
-busqueda.addEventListener("change", obtenerEvento);
+// busqueda.addEventListener("input", obtenerEvento);
+// busqueda.addEventListener("change", obtenerEvento);
 
-function obtenerEvento(e) {
-  console.log(`EVENTO: ${e.type}`);
-}
+// function obtenerEvento(e) {
+//   console.log(`EVENTO: ${e.type}`);
+// }
