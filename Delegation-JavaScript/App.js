944352fd805311769f8d748b1fa896123aee1edd
agusahumada <agustinaahumada649@gmail.com>
2020-07-05 01:01:48 -0300

    Delegation-Js

---
+++ Delegation-JavaScript/App.js
@@ -0,0 +1,14 @@
+//Delegation
+
+document.body.addEventListener("click", eliminarElemento);
+
+function eliminarElemento(e) {
+  e.preventDeafault;
+
+  if (e.target.classList.contains("borrar-curso")) {
+    console.log(e.target.parentElement.parentElement.remove());
+  }
+  if (e.target.classList.contains("agregar-carrito")) {
+    console.log("Curso agregado");
+  }
+}
--- Event-Bubbling/App.js
+++ Event-Bubbling/App.js
@@ -1,23 +1,23 @@
-// Event bubbling
+// // Event bubbling
 
-const card = document.querySelector(".card");
-const infoCurso = document.querySelector(".info-card");
-const agregarCarrito = document.querySelector(".agregar-carrito");
+// const card = document.querySelector(".card");
+// const infoCurso = document.querySelector(".info-card");
+// const agregarCarrito = document.querySelector(".agregar-carrito");
 
-card.addEventListener("click", function (e) {
-  console.log("Click en card");
+// card.addEventListener("click", function (e) {
+//   console.log("Click en card");
 
-  //Para detener la propagacion
+//   //Para detener la propagacion
 
-  e.stopPropagation();
-});
+//   e.stopPropagation();
+// });
 
-infoCurso.addEventListener("click", function (e) {
-  console.log("Click en info Curso");
-  e.stopPropagation();
-});
+// infoCurso.addEventListener("click", function (e) {
+//   console.log("Click en info Curso");
+//   e.stopPropagation();
+// });
 
-agregarCarrito.addEventListener("click", function (e) {
-  console.log("Click en agregar a carrito");
-  e.stopPropagation();
-});
+// agregarCarrito.addEventListener("click", function (e) {
+//   console.log("Click en agregar a carrito");
+//   e.stopPropagation();
+// });
