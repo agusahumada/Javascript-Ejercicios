b70c650b8e62f4320b26b06d996baaf0c98684ab
agusahumada <agustinaahumada649@gmail.com>
2020-07-06 23:51:28 -0300

    Local Storage

--- Delegation-JavaScript/App.js
+++ Delegation-JavaScript/App.js
@@ -1,14 +1,14 @@
-//Delegation
+// //Delegation
 
-document.body.addEventListener("click", eliminarElemento);
+// document.body.addEventListener("click", eliminarElemento);
 
-function eliminarElemento(e) {
-  e.preventDeafault;
+// function eliminarElemento(e) {
+//   e.preventDeafault;
 
-  if (e.target.classList.contains("borrar-curso")) {
-    console.log(e.target.parentElement.parentElement.remove());
-  }
-  if (e.target.classList.contains("agregar-carrito")) {
-    console.log("Curso agregado");
-  }
-}
+//   if (e.target.classList.contains("borrar-curso")) {
+//     console.log(e.target.parentElement.parentElement.remove());
+//   }
+//   if (e.target.classList.contains("agregar-carrito")) {
+//     console.log("Curso agregado");
+//   }
+// }
