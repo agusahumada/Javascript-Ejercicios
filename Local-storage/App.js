f3023dfbf43c67fa11f5aae780fd52c0baf9859f
agusahumada <agustinaahumada649@gmail.com>
2020-07-07 23:04:59 -0300

    Agregando y borrando LocalStorage

---
+++ Local-storage/App.js
@@ -0,0 +1,11 @@
+//Local storage
+const nombre = localStorage.setItem("nombre", "Juan");
+console.log(nombre);
+
+//Session Storage
+sessionStorage.setItem("nombre", "juan");
+
+// //Eliminar de Local Storage
+// localStorage.removeItem("nombre");
+
+localStorage.clear();
